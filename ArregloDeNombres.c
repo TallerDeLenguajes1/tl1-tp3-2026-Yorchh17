cbe899c3653ecb75e811afae3373c0e79494c431
Jose <jorgejose331gmail.com>
2026-04-17 13:58:30 -0300

    Antes del merch

--- ArregloDeNombres.c
+++ ArregloDeNombres.c
@@ -2,13 +2,14 @@
 #include<string.h>
 #include<stdlib.h>
 void mostrarPersonas(char *ArreDeNombres[]);
-
+int BuscarNombre(char *buscar,char *ArreDeNombres[]);
 int main(){
+
 
 printf("Ingrese 5 nombres de persona: \n");
 char *arreglosNombres[5];
 char nombre[100];
-
+char buscar[20];
 for (int i = 0; i < 5; i++)
 {
     scanf("%s", nombre);
@@ -17,6 +18,16 @@ for (int i = 0; i < 5; i++)
     strcpy(arreglosNombres[i],nombre);
 }
 mostrarPersonas(arreglosNombres);
+
+printf("Ingrese la palabra a buscar \n");
+scanf("%s",buscar);
+int indice=BuscarNombre(buscar,arreglosNombres);
+if (indice==-1)
+{
+    printf("No se encontro el nombre \n");
+}else{
+    printf("El nombre encontrado es : \n %s", arreglosNombres[indice]);
+}
 
     return 0;
 }
@@ -27,6 +38,15 @@ void mostrarPersonas(char  *ArreDeNombres[]){
     {
         printf("%s\n",ArreDeNombres[i]);
     }
-    
-
 }
+
+int BuscarNombre(char *buscar, char *ArreDeNombres[]) {
+    for (int i = 0; i < 5; i++) {
+        
+        if (strstr(ArreDeNombres[i], buscar) != NULL) {
+            return i; 
+        }
+    }
+    return -1;
+}
+
